b5b259fec8defb96aae132bbbd52f2c956c8688e
Arth <arthur99810@gmail.com>
2024-03-15 15:11:04 -0300

    edit 1 - Mensagem de "nenhum extrato/saque foi feito"

--- desafios/desafio_banco.py
+++ desafios/desafio_banco.py
@@ -7,6 +7,7 @@ menu = '''
 
 ==>   '''
 
+numero_depositos = 0
 totalDepositado = 0
 totalSacado = 0
 saldo = 0
@@ -24,6 +25,7 @@ while True:
         if valorDeposito >0:
             saldo = valorDeposito + saldo
             totalDepositado = valorDeposito + totalDepositado
+            numero_depositos += 1
             print("Deposito feito com Sucesso \n Novo saldo de: R$ {:.2f}".format(saldo))
         else:
             print("Não foi possivel realizar o depósito, por favor insira um valor correto")
@@ -50,10 +52,29 @@ while True:
                 print("Voce nao pode fazer um saque com um valor igual a 0")
         
     elif opcao =="e":
-        print("extrato")
-        print("Saldo: R${:.2f}".format(saldo))
-        print("Valor dos saques feitos: R${:.2f}      {}/{}".format(totalSacado,numero_saques,LIMITE_SAQUES))
-        print("Valor total depositado: R${:.2f}".format(totalDepositado))
+        if numero_depositos == 0:
+            if numero_saques == 0:
+                print("extrato")
+                print("Ainda não foi feito nenhum saque")
+                print("Ainda não foi feito nenhum depósito")
+                print("Saldo: R${:.2f}".format(saldo))
+            elif numero_depositos==0:
+                print("extrato")
+                print("Valor total sacado: R${:.2f}".format(totalSacado))
+                print("Ainda não foi feito nenhum deposito")
+                print("Saldo: R${:.2f}".format(saldo))
+        elif numero_saques == 0:
+            print("extrato")
+            print("Ainda não foi feito nenhum saque")
+            print("Valor total depositado: R${:.2f}".format(totalDepositado))
+            print("Saldo: R${:.2f}".format(saldo))
+        else:
+            print("extrato")
+            print("Valor dos saques feitos: R${:.2f}      {}/{}".format(totalSacado,numero_saques,LIMITE_SAQUES))
+            print("Valor total depositado: R${:.2f}".format(totalDepositado))
+            print("Saldo: R${:.2f}".format(saldo))
+        
+
     elif opcao == "q":
         print("Saindo")
         break
